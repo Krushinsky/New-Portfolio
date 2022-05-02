7214a183d30f0ba0aa0f335da1bb6a14b0007080
Krushinsky <tj_chel@mail.ru>
2022-05-02 17:07:18 +0500

    add cursor at github

--- js/preloader.js
+++ js/preloader.js
@@ -5,3 +5,10 @@ window.onload = function () {
       document.body.classList.remove('loaded_hiding');
     }, 500);
   }
+
+  let github = document.querySelector('.github');
+  github.addEventListener('click', ()=>{
+    let url = 'https://github.com/Krushinsky?tab=repositories';
+    window.open(url, '_blank');
+    
+  })
